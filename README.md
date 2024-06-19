b92140cb990e790a24eb82760cbb6b39f8115a2d
GitHub Action <action@github.com>
2024-06-20 00:50:07 +0900

    add: add graph utilities and more

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 ```
 シンプルなコンパイル
 gcc sample.c -o sample && ./sample
-
 他のコンパイル
 gcc panel.c -o panel -lncurses -lpanel && ./panel
-
 gcc `pkg-config --cflags gtk+-3.0` gtk_window.c -o gtk_window `pkg-config --libs gtk+-3.0`
+gcc $(pkg-config --cflags gtk+-3.0 plplot) gtk_window.c -o gtk_window $(pkg-config --libs gtk+-3.0 plplot)
 ./gtk_window
 
 このディレクトリで、UbuntuやDebian系のディストリビューションでよく使用される開発ツールとライブラリをインストール実行
-
 sudo apt-get update
 sudo apt-get install build-essential libncurses5-dev libncursesw5-dev git cmake pkg-config
 sudo apt-get install libgtk-3-dev
+sudo apt-get install libplplot-dev
+sudo apt-get install libplplot-dev plplot-driver-cairo
 ```
--- gtk_window.c
+++ gtk_window.c
@@ -1,15 +1,77 @@
 #include <gtk/gtk.h>
+#include <plplot.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-    GtkWidget *window;
-    gtk_init(&argc, &argv);
+#define MAX_POINTS 1000
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+double x[MAX_POINTS];
+double y[MAX_POINTS];
+int num_points = 0;
+char xlabel[100], ylabel[100]; // ラベル名を格納するための変数
 
-    gtk_widget_show_all(window);
-    gtk_main();
+void load_csv_data(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("File opening failed");
+        return;
+    }
+
+    // ラベル名の読み込み
+    if (fscanf(file, "%99[^,],%99[^\n]\n", xlabel, ylabel) != 2) {
+        perror("Failed to read labels");
+        fclose(file);
+        return;
+    }
+
+    printf("num_points: %d\n", num_points);
+    printf("MAX_POINTS: %d\n", MAX_POINTS);
+    
+    while (fscanf(file, "%lf,%lf", &x[num_points], &y[num_points]) == 2 && num_points < MAX_POINTS) {
+        num_points++;
+    }
+
+    fclose(file);
+}
+
+// PLplot の描画を行う関数
+void plot_data() {
+    //plinit();
+    plenv(0.0, 13.0, 125000000.0, 127000000.0, 0, 0); // 軸の範囲を設定
+    pllab(xlabel, ylabel, ""); // 軸のラベルを設定
+    //plcol0(1); // 色を設定
+    plline(num_points, x, y); // データのプロット
+    plend();
+}
+
+// GTK+ の描画エリアの "draw" イベントに対するコールバック関数
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+    plot_data(); // PLplot を使用してデータをプロット
+    return FALSE;
+}
+
+// アプリケーションの終了時に plend を呼び出すためのコールバック関数
+void on_app_destroy(GtkWidget *widget, gpointer data) {
+    //plend(); // PLplot の終了処理
+    gtk_main_quit();
+}
+
+int main(int argc, char *argv[]) {
+    printf("Hello, world!\n");
+
+    // CSVデータの読み込み
+    load_csv_data("sample_data/sample.csv");
+
+    // PLplot の初期化をここで行う
+    plsdev("xcairo"); // 外部 Cairo デバイスドライバを使用
+    plinit();
+
+    // PLplot を使用してデータをプロット
+    plot_data();
+
+    // PLplot の終了処理
+    plend();
 
     return 0;
 }
+
